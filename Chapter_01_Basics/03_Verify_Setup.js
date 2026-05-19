2bbf690e4fda9991036e6463e88bd5070a8f2ff4
Mailesh-B <bmailesh2020@gmail.com>
2026-05-19 12:11:48 +0530

    Identifier rules part 1

---
+++ Chapter_01_Basics/03_Verify_Setup.js
@@ -0,0 +1,7 @@
+console.log(process.platform); // To verify the OS
+
+console.log(process.arch); // To verify the OS Architecture
+
+console.log("Node Version:",process.version); //To verify node version installed   
+
+
---
+++ Chapter_03_Identifier_Literals/06_Identifier_Rules.js
@@ -0,0 +1,19 @@
+//Valid Variables
+var m = 20;
+var _s = 30;
+var $b = 40;
+var m9 = 50;
+let M;
+M = 15;
+console.log(M);
+
+//variables are case sensitive
+var Mail = 90;
+var mail = 100;
+console.log(Mail);
+console.log(mail);
+
+
+//Invalid variables
+var 23a = 60;
+console.log(23a);
